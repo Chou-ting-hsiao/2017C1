8b6e0da8350f6c35a3b1058cee35d1cf38ebb7ae
Your Name <you@example.com>
2017-03-01 15:54:01 +0800

    0301

--- HomeWork003/Form1.Designer.cs
+++ HomeWork003/Form1.Designer.cs
@@ -36,6 +36,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,10 +53,10 @@
             this.name,
             this.tel,
             this.address});
-            this.dataGridView1.Location = new System.Drawing.Point(34, 22);
+            this.dataGridView1.Location = new System.Drawing.Point(38, 26);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(442, 174);
+            this.dataGridView1.Size = new System.Drawing.Size(443, 168);
             this.dataGridView1.TabIndex = 0;
             // 
             // seq
@@ -79,14 +85,16 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 240);
+            this.textBox1.Location = new System.Drawing.Point(122, 259);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            this.textBox1.WordWrap = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(314, 239);
+            this.button1.Location = new System.Drawing.Point(301, 225);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -98,21 +106,88 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(78, 241);
+            this.label1.Location = new System.Drawing.Point(35, 259);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "電話:";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::HomeWork003.Properties.Resources.folder_profiles_icon_4176;
+            this.button2.Location = new System.Drawing.Point(412, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 40);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "新增";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(35, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "名字:";
+            this.label2.UseWaitCursor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 219);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(35, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "地址:";
+            this.label3.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 294);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Visible = false;
+            this.textBox3.WordWrap = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(511, 323);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -131,6 +206,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- HomeWork003/Form1.cs
+++ HomeWork003/Form1.cs
@@ -14,16 +14,22 @@ namespace HomeWork003
     public partial class Form1 : Form
     {
         private List<phone> list;
+        string fileName = "phone.csv";
+        int i = 0;
         public Form1()
         {
             InitializeComponent();
+            gvInit();
+        }
+
+        public void gvInit()
+        {
             list = CreateData();
             dataGridView1.DataSource = list;
         }
         private List<phone> CreateData()
         {
             char[] split = new char[] { ',' };
-            string fileName = "phone.csv";
             List<phone> result = new List<phone>();
             if (File.Exists(fileName))
             {
@@ -50,23 +56,78 @@ namespace HomeWork003
       
         private void button1_Click(object sender, EventArgs e)
         {
-            var search = list.Where((x) => x.Tel == textBox1.Text);
-
             /* List<phone> result = new List<phone>();
-             foreach (var item in search)
-             {
-                 var phone = new phone
-                 {
-                     Seq = item.Seq,
-                     Name = item.Name,
-                     Tel = item.Tel,
-                     Address = item.Address
-                 };
-                 result.Add(phone);
-                 // MessageBox.Show(item.Seq+","+item.Name + "," + item.Tel + "," + item.Address);
-             }
-             */
-            dataGridView1.DataSource = search.ToList();
+            foreach (var item in search)
+            {
+                var phone = new phone
+                {
+                    Seq = item.Seq,
+                    Name = item.Name,
+                    Tel = item.Tel,
+                    Address = item.Address
+                };
+                result.Add(phone);
+                // MessageBox.Show(item.Seq+","+item.Name + "," + item.Tel + "," + item.Address);
+            }
+            */
+            char[] split = new char[] { ',' };
+            List<phone> result = new List<phone>();
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                for (int i = 1; i < lines.Count(); i++)
+                {
+                    string[] items = lines[i].Split(split);
+
+                    if (items[1]==textBox2.Text)
+                    {
+                      
+                        MessageBox.Show(items[0] + "," + items[1] + "," + items[2] + "," + items[3]);
+                    }
+                 
+                }
+            }
+            //var search = list.Where((x) => x.Name == textBox2.Text);
+           // dataGridView1.DataSource = search.ToList();
+            //textBox1.Text = "";
+        }
+
+      
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            gvInit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (i==0)
+            {
+                label1.Visible = true;
+                label3.Visible = true;
+                textBox1.Visible = true;
+                textBox3.Visible = true;
+                i++;
+            }
+            else
+            {
+                if (File.Exists(fileName))
+                {
+                    //File.AppendAllText(fileName,list.Count()+1.ToString()+","+"555" + "," + "5555" + "," + "5555");
+                    File.AppendAllText(fileName, "\r\n" + Convert.ToString(list.Count() + 1) + "," + textBox2.Text + "," + textBox1.Text + "," + textBox3.Text, Encoding.Unicode);
+                  
+                    gvInit();
+                }
+                else
+                {
+                    MessageBox.Show("檔案不存在");
+                }
+                label1.Visible = false;
+                label3.Visible = false;
+                textBox1.Visible = false;
+                textBox3.Visible =false;
+                i = 0;
+            }
+          
         }
     }
 }
